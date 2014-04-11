f27d8d1ff007488a01ef1a1cdc4f8c7271c973c3
oneoo <oneoo@yo2.com>
2014-04-11 16:50:30 +0800

    add route.lua example

--- route.lua
+++ route.lua
@@ -17,6 +17,29 @@ others you want :)
 ]]
 
 if not router(headers.uri, routes) then
-    header('HTTP/1.1 404 Not Found')
-    echo('File Not Found!')
+    -- try local scripts
+    --[[
+        /       =>  /index.lua
+        /hello  =>  /hello.lua  if not exists then try /hello/index.lua
+        /hello/ =>  /hello.lua  if not exists then try /hello/index.lua
+    ]]
+    local file = headers.uri
+    if file:byte(#file) == 47 then -- end with /
+        file = file:sub(1, #file - 1)
+    end
+
+    if file_exists(file .. '.lua') then
+        file = file .. '.lua'
+    elseif file_exists(file .. '/index.lua') then
+        file = file .. '/index.lua'
+    else
+        file = nil
+    end
+
+    if file and file ~= '/route.lua' then
+        dofile(file)
+    else
+        header('HTTP/1.1 404 Not Found')
+        echo('File Not Found!')
+    end
 end
