584884deb8ac62ef49f7e11e1b893ff56369ea77
oneoo <oneoo@yo2.com>
2014-04-14 14:27:45 +0800

    fix router example

--- route.lua
+++ route.lua
@@ -8,9 +8,9 @@ routes['^/user/:user_id'] = function(r)
     print('User ID: ', r.user_id)
 end
 
-routes['^/user/:user_id/:post'] = function(r)
+routes['^/user/:user_id/:post(.+)'] = function(r)
     print('User ID: ', r.user_id)
-    print('\nPost: ', r.post)
+    print(' Post: ', r.post)
 end
 
 routes['^/(.*)'] = function(r)
@@ -21,13 +21,8 @@ end
 others you want :)
 ]]
 
+-- if the 3rd argument is a path, then router will try to get local lua script file first
 if not router(headers.uri, routes, '/') then
-    -- try local scripts
-    --[[
-        /       =>  /index.lua
-        /hello  =>  /hello.lua  if not exists then try /hello/index.lua
-        /hello/ =>  /hello.lua  if not exists then try /hello/index.lua
-    ]]
     header('HTTP/1.1 404 Not Found')
     echo('File Not Found!')
 end
